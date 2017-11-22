ed67b8255d6c14286b91ed57bba062a1cd915198
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 02:43:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,17 +6,3 @@
           is_prime = false
       end
     end
-
-
-
-
-
-
-
-
-  return false if number < 2
-     (2..Math.sqrt(number)).each do |num|
-      return false if number % num == 0
-    end
-    true
-  end
