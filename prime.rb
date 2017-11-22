44d6a6fa0f62f4a4f485357e6ddab1ac44fc3906
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 02:16:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,13 @@
 # Add  code here!
 def prime? (num)
-  for i in 2..num-1
-    return true
-    if num % i == 0
-     return false
+
+
+    for random in 2..(num - 1)
+     if (num % random) == 0
+      return false
+     end
     end
+    true
+    end
+
+    
