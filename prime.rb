0eea6ffd521a420fe808536e24ad215e6c4bf138
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 02:19:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 # Add  code here!
 def prime? (num)
-  (2..(num - 1)).each do |n|
-    return false if num % n == 0
+  for random in 2..(num - 1)
+   if (num % random) == 0
+    return false
+   end
   end
   true
-end
+  end
 
+  
   
