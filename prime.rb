dc45e3fe71edf4a05b824c359560a7fcd8d559a4
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 02:46:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,21 @@
 # Add  code here!
   def prime?(n)
+ # starting with 2 because testing for a prime means you don't want to test division by 1
+      2.upto(Math.sqrt(n)) do |x| # testing up to the square root of the number because going past there is excessive
+        if n % x == 0
+          # n is the number being called from the program
+          # x is the number we're dividing by, counting from 2 up to the square root of the number
+          return false # this means the number is not prime
+        else
+          return true # this means the number is prime
+        end
+      end
+    end
+
+
+
+
+
     if n <= 1
      false
  elsif n == 2
