b904aa76917fcac09599723f070a07deb81d2056
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-22 02:42:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,20 @@
 # Add  code here!
   def prime?(number)
-    if number < 2
-    return false
-  else
+      is_prime = true
+      for i in 2..number-1
+        if nnumber % i == 0
+          is_prime = false
+      end
+    end
+
+
+
+
+
+
+
+
+  return false if number < 2
      (2..Math.sqrt(number)).each do |num|
       return false if number % num == 0
     end
